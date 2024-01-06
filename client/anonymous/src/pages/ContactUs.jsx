dae43ca597860e7b784f3caf7d71642735ceeacb
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-01-06 11:05:49 +0530

    contact us animation implemented

--- client/anonymous/src/pages/ContactUs.jsx
+++ client/anonymous/src/pages/ContactUs.jsx
@@ -1,39 +1,58 @@
 import React from "react";
-import contactimg from "../assets/contactimg.png";
-import "../css files/ContactUs.css"
+import Lottie from "react-lottie";
+import contactAnimation from "../json files/contact-us.json"; // Adjust the path accordingly
+import "../css files/ContactUs.css";
 
 function ContactUs() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: contactAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
     <div className="container-fluid">
-      <div class="heading_container">
-            <h2>Contact Us</h2>
+      <div className="heading_container">
+        <h2>Contact Us</h2>
       </div>
-      <div class="row px-3 py-5">
-        <div class="col-md-6 contact_section">
+      <div className="row px-3 py-5">
+        <div className="col-md-6 contact_section">
           <form action="#" method="post">
-          <div>
-            <input type="text" placeholder="Name" fdprocessedid="6gbrm"/>
-          </div>
-          <div>
-            <input type="text" placeholder="Phone Number" fdprocessedid="9ykr8n"/>
-          </div>
-          <div>
-            <input type="email" placeholder="Email" fdprocessedid="ifgt3c"/>
-          </div>
-          <div>
-            <input type="text" class="message-box" placeholder="Message" fdprocessedid="kedw1d"/>
-          </div>
-          <div class="d-flex">
-            <button fdprocessedid="leew5l">SEND</button>
-          </div>
+            <div>
+              <input type="text" placeholder="Name" fdprocessedid="6gbrm" />
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="Phone Number"
+                fdprocessedid="9ykr8n"
+              />
+            </div>
+            <div>
+              <input type="email" placeholder="Email" fdprocessedid="ifgt3c" />
+            </div>
+            <div>
+              <input
+                type="text"
+                className="message-box"
+                placeholder="Message"
+                fdprocessedid="kedw1d"
+              />
+            </div>
+            <div className="d-flex">
+              <button fdprocessedid="leew5l">SEND</button>
+            </div>
           </form>
         </div>
         <div className="col-md-6 myimg">
-          <img src={contactimg}/>
+          <Lottie options={defaultOptions} height={500} width={500} />
         </div>
       </div>
     </div>
-);
+  );
 }
 
 export default ContactUs;
