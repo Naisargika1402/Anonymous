a2349ba1558bed9033d4aaf1287f91786dc863eb
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-01-10 00:36:09 +0530

    Services page created

--- client/anonymous/src/pages/Services.jsx
+++ client/anonymous/src/pages/Services.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function Services() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Header />
+      <Footer />
     </div>
   );
 }
