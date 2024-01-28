af24af13ea978837f84b9d11fa9963e045533478
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-01-28 12:16:17 +0530

    choose ussection modified

--- client/anonymous/src/components/ChooseUs.jsx
+++ client/anonymous/src/components/ChooseUs.jsx
@@ -1,68 +1,93 @@
-import React,{useState} from "react";
-import ScrollTrigger from 'react-scroll-trigger';
-import flxb from "../assets/flxb.png";
-import optns from "../assets/optns.png";
-import prof from "../assets/prof.png";
-import qlty from "../assets/qlty.png";
+import React, { useState } from "react";
+import ScrollTrigger from "react-scroll-trigger";
+import {
+  FaClock,
+  FaCog,
+  FaUser,
+  FaShieldAlt,
+  FaClipboardList,
+  FaDollarSign,
+} from "react-icons/fa";
 import "../css files/ChooseUs.css";
 
 const ChooseUs = () => {
-    const[anmtn,setAnmtn] = useState(false);
-    return(
-        <ScrollTrigger onEnter={()=>setAnmtn(true)}>
-        <h2 style={{textAlign:"center"}}>Why Choose Us</h2>
-        {anmtn && <div className="timeline">
-            <div className="mycontnr left-cntr">
-                <img src={flxb}></img>
-                <div className="txt-bx">
-                <h3>Flexible Schedule</h3>  
-                <p>Sem in eget faucibus massa arcu et elementum sed massa nulla dolor felis dictumst malesuada non.</p>
-                <span className="left-arrow"></span>
-                </div>
+  const [anmtn, setAnmtn] = useState(false);
+
+  return (
+    <ScrollTrigger onEnter={() => setAnmtn(true)}>
+      <h2 style={{ textAlign: "center" }}>Why Choose Us</h2>
+      {anmtn && (
+        <div className="timeline">
+          <div className="mycontnr left-cntr">
+            <FaClock size={35} className="left-img" />
+            <div className="txt-bx">
+              <h3>Flexible Schedule</h3>
+              <p>
+                Sem in eget faucibus massa arcu et elementum sed massa nulla
+                dolor felis dictumst malesuada non.
+              </p>
+              <span className="left-arrow"></span>
             </div>
-            <div className="mycontnr right-cntr">
-                <img src={optns}></img>
-                <div className="txt-bx">
-                <h3>Multiple Options</h3>  
-                <p>Sem in eget faucibus massa arcu et elementum sed massa nulla dolor felis dictumst malesuada non.</p>
-                <span className="right-arrow"></span>
-                </div>
+          </div>
+          <div className="mycontnr right-cntr">
+            <FaCog size={35} className="right-img" />
+            <div className="txt-bx">
+              <h3>Multiple Options</h3>
+              <p>
+                Sem in eget faucibus massa arcu et elementum sed massa nulla
+                dolor felis dictumst malesuada non.
+              </p>
+              <span className="right-arrow"></span>
             </div>
-            <div className="mycontnr left-cntr">
-                <img src={prof}></img>
-                <div className="txt-bx">
-                <h3>Top Quality Service</h3>  
-                <p>Sem in eget faucibus massa arcu et elementum sed massa nulla dolor felis dictumst malesuada non.</p>
-                <span className="left-arrow"></span>
-                </div>
+          </div>
+          <div className="mycontnr left-cntr">
+            <FaUser size={35} className="left-img" />
+            <div className="txt-bx">
+              <h3>Top Quality Service</h3>
+              <p>
+                Sem in eget faucibus massa arcu et elementum sed massa nulla
+                dolor felis dictumst malesuada non.
+              </p>
+              <span className="left-arrow"></span>
             </div>
-            <div className="mycontnr right-cntr">
-                <img src={qlty}></img>
-                <div className="txt-bx">
-                <h3>Licensed Professional</h3>  
-                <p>Sem in eget faucibus massa arcu et elementum sed massa nulla dolor felis dictumst malesuada non.</p>
-                <span className="right-arrow"></span>
-                </div>
+          </div>
+          <div className="mycontnr right-cntr">
+            <FaShieldAlt size={35} className="right-img" />
+            <div className="txt-bx">
+              <h3>Licensed Professional</h3>
+              <p>
+                Sem in eget faucibus massa arcu et elementum sed massa nulla
+                dolor felis dictumst malesuada non.
+              </p>
+              <span className="right-arrow"></span>
             </div>
-            <div className="mycontnr left-cntr">
-                <img src={qlty}></img>
-                <div className="txt-bx">
-                <h3>Monthly Servies Subscriptions</h3>  
-                <p>Sem in eget faucibus massa arcu et elementum sed massa nulla dolor felis dictumst malesuada non.</p>
-                <span className="left-arrow"></span>
-                </div>
+          </div>
+          <div className="mycontnr left-cntr">
+            <FaClipboardList size={35} className="left-img" />
+            <div className="txt-bx">
+              <h3>Monthly Servies Subscriptions</h3>
+              <p>
+                Sem in eget faucibus massa arcu et elementum sed massa nulla
+                dolor felis dictumst malesuada non.
+              </p>
+              <span className="left-arrow"></span>
             </div>
-            <div className="mycontnr right-cntr">
-                <img src={qlty}></img>
-                <div className="txt-bx">
-                <h3>Best Price Packages</h3>  
-                <p>Sem in eget faucibus massa arcu et elementum sed massa nulla dolor felis dictumst malesuada non.</p>
-                <span className="right-arrow"></span>
-                </div>
+          </div>
+          <div className="mycontnr right-cntr">
+            <FaDollarSign size={35} className="right-img" />
+            <div className="txt-bx">
+              <h3>Best Price Packages</h3>
+              <p>
+                Sem in eget faucibus massa arcu et elementum sed massa nulla
+                dolor felis dictumst malesuada non.
+              </p>
+              <span className="right-arrow"></span>
             </div>
-        </div>}
-        </ScrollTrigger>
-    )
-}
+          </div>
+        </div>
+      )}
+    </ScrollTrigger>
+  );
+};
 
 export default ChooseUs;
--- client/anonymous/src/pages/UserPage.jsx
+++ client/anonymous/src/pages/UserPage.jsx
@@ -1,28 +1,27 @@
-import React, {useEffect, useRef, useState} from 'react'
-import UserHeader from './UserHeader';
-import HomeServices from '../components/HomeServices';
-import Container from '../styles/Container';
+import React, { useEffect, useRef, useState } from "react";
+import UserHeader from "./UserHeader";
+import HomeServices from "../components/HomeServices";
+import Container from "../styles/Container";
 import "../css files/UserPage.css";
-import { Search } from "@material-ui/icons"
+import { Search } from "@material-ui/icons";
 
-const apiKey = 'AIzaSyDKc-PxciHbJWqufj9dakp14I9aCuxw1oc';
-const mapApiJs = 'https://maps.googleapis.com/maps/api/js';
-const geocodeJson = 'https://maps.googleapis.com/maps/api/geocode/json';
-
+const apiKey = "AIzaSyDKc-PxciHbJWqufj9dakp14I9aCuxw1oc";
+const mapApiJs = "https://maps.googleapis.com/maps/api/js";
+const geocodeJson = "https://maps.googleapis.com/maps/api/geocode/json";
 
 // load google map api js
 
 function loadAsyncScript(src) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     const script = document.createElement("script");
     Object.assign(script, {
       type: "text/javascript",
       async: true,
-      src
-    })
+      src,
+    });
     script.addEventListener("load", () => resolve(script));
     document.head.appendChild(script);
-  })
+  });
 }
 
 const extractAddress = (place) => {
@@ -36,30 +35,30 @@ const extractAddress = (place) => {
     zip: "",
     country: "",
     plain() {
-      const locality = this.locality ? this.locality+ ", " : "";
+      const locality = this.locality ? this.locality + ", " : "";
       const city = this.city ? this.city + ", " : "";
       const state = this.state ? this.state + ", " : "";
       return locality + city + state + this.country;
-    }
-  }
+    },
+  };
 
   if (!Array.isArray(place?.address_components)) {
     return address;
   }
 
-  place.address_components.forEach(component => {
+  place.address_components.forEach((component) => {
     const types = component.types;
     const value = component.long_name;
 
-    if(types.includes("plus_code")){
+    if (types.includes("plus_code")) {
       address.plot = value;
     }
 
-    if(types.includes("sublocality_level_2")){
+    if (types.includes("sublocality_level_2")) {
       address.locality = value;
     }
 
-    if(types.includes("sublocality_level_1")){
+    if (types.includes("sublocality_level_1")) {
       address.zone = value;
     }
 
@@ -78,88 +77,84 @@ const extractAddress = (place) => {
     if (types.includes("country")) {
       address.country = value;
     }
-
   });
 
   return address;
-}
-
+};
 
 function UserPage() {
-
   const searchInput = useRef(null);
   const [address, setAddress] = useState({});
-
 
   // init gmap script
   const initMapScript = () => {
     // if script already loaded
-    if(window.google) {
+    if (window.google) {
       return Promise.resolve();
     }
     const src = `${mapApiJs}?key=${apiKey}&libraries=places&v=weekly`;
     return loadAsyncScript(src);
-  }
+  };
 
   // do something on address change
   const onChangeAddress = (autocomplete) => {
     const place = autocomplete.getPlace();
     setAddress(extractAddress(place));
-  }
+  };
 
   // init autocomplete
   const initAutocomplete = () => {
     if (!searchInput.current) return;
 
-    const autocomplete = new window.google.maps.places.Autocomplete(searchInput.current);
+    const autocomplete = new window.google.maps.places.Autocomplete(
+      searchInput.current
+    );
     autocomplete.setFields(["address_component", "geometry"]);
-    autocomplete.addListener("place_changed", () => onChangeAddress(autocomplete));
+    autocomplete.addListener("place_changed", () =>
+      onChangeAddress(autocomplete)
+    );
+  };
 
-  }
-
-
-  const reverseGeocode = ({ latitude: lat, longitude: lng}) => {
+  const reverseGeocode = ({ latitude: lat, longitude: lng }) => {
     const url = `${geocodeJson}?key=${apiKey}&latlng=${lat},${lng}`;
     searchInput.current.value = "Getting your location...";
     fetch(url)
-        .then(response => response.json())
-        .then(location => {
-          const place = location.results[0];
-          const _address = extractAddress(place);
-          setAddress(_address);
-          searchInput.current.value = _address.plain();
-        })
-  }
-
+      .then((response) => response.json())
+      .then((location) => {
+        const place = location.results[0];
+        const _address = extractAddress(place);
+        setAddress(_address);
+        searchInput.current.value = _address.plain();
+      });
+  };
 
   const findMyLocation = () => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-        reverseGeocode(position.coords)
-      })
+      navigator.geolocation.getCurrentPosition((position) => {
+        reverseGeocode(position.coords);
+      });
     }
-  }
+  };
 
   // load map script after mounted
   useEffect(() => {
-    initMapScript().then(() => initAutocomplete())
+    initMapScript().then(() => initAutocomplete());
   }, []);
 
-
-
-  return(
+  return (
     <Container>
-      <UserHeader searchInput={searchInput} findMyLocation={findMyLocation}></UserHeader>
-      <h1 className='userheading'>Services You're Looking For...</h1>
-      <div className='mainsearch'>
-        <input placeholder='search for services'
-        />
+      <UserHeader
+        searchInput={searchInput}
+        findMyLocation={findMyLocation}
+      ></UserHeader>
+      <h1 className="userheading">Services You're Looking For...</h1>
+      <div className="mainsearch">
+        <input placeholder="search for services" />
         <Search></Search>
       </div>
       <HomeServices></HomeServices>
     </Container>
-  )
-  
+  );
 }
 
 export default UserPage;
