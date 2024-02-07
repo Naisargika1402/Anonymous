56ca4faaae4ef4a048fd0c83af25bd62225fb589
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-02-07 09:59:34 +0530

    Update SignUpRedirect.jsx

--- client/anonymous/src/pages/SignUpRedirect.jsx
+++ client/anonymous/src/pages/SignUpRedirect.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaUserTie, FaUser } from "react-icons/fa";
 import "../css files/SignUpRedirect.css";
+import Header from "../components/Header";
 
 function SignUpRedirect() {
   const handleBoxClick = (role) => {
@@ -10,31 +11,36 @@ function SignUpRedirect() {
   };
 
   return (
-    <div className="signup-container">
-      <h1 className="signup-header">Hola, please select your role.</h1>
-      <p className="signup-description">
-        We'll personalize your setup experience accordingly.
-      </p>
-      <br />
-      <br />
-      <div>
-        <Link
-          to="/signup-employee"
-          className="signup-box"
-          onClick={() => handleBoxClick("employee")}
-        >
-          <FaUserTie style={{ marginRight: "8px", verticalAlign: "middle" }} />
-          I'm here to provide services
-        </Link>
+    <div>
+      <Header />
+      <div className="signup-container">
+        <h1 className="signup-header">Hola, please select your role.</h1>
+        <p className="signup-description">
+          We'll personalize your setup experience accordingly.
+        </p>
+        <br />
+        <br />
+        <div>
+          <Link
+            to="/signup-employee"
+            className="signup-box"
+            onClick={() => handleBoxClick("employee")}
+          >
+            <FaUserTie
+              style={{ marginRight: "8px", verticalAlign: "middle" }}
+            />
+            I'm here to provide services
+          </Link>
 
-        <Link
-          to="/signup-user"
-          className="signup-box"
-          onClick={() => handleBoxClick("customer")}
-        >
-          <FaUser style={{ marginRight: "8px", verticalAlign: "middle" }} />
-          I'm looking for services
-        </Link>
+          <Link
+            to="/signup-user"
+            className="signup-box"
+            onClick={() => handleBoxClick("customer")}
+          >
+            <FaUser style={{ marginRight: "8px", verticalAlign: "middle" }} />
+            I'm looking for services
+          </Link>
+        </div>
       </div>
     </div>
   );
